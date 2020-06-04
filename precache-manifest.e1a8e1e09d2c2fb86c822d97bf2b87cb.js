436bbe331b876a59b6abac82a73b142d28ea70d2
akshayDatacode <[akshaywebllisto@gmail.com]>
2020-06-04 16:24:01 +0530

    Updates

---
+++ precache-manifest.e1a8e1e09d2c2fb86c822d97bf2b87cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef006ce5f2a3f0002704522c9275067",
+    "url": "/wow-tutors/index.html"
+  },
+  {
+    "revision": "3e97f1884422f4d3b3ad",
+    "url": "/wow-tutors/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f2f862ba569d83ee0292",
+    "url": "/wow-tutors/static/css/main.af7c0659.chunk.css"
+  },
+  {
+    "revision": "3e97f1884422f4d3b3ad",
+    "url": "/wow-tutors/static/js/2.74f188fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wow-tutors/static/js/2.74f188fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f862ba569d83ee0292",
+    "url": "/wow-tutors/static/js/main.fa573bfa.chunk.js"
+  },
+  {
+    "revision": "7fed6f19e39f3609b0ad",
+    "url": "/wow-tutors/static/js/runtime-main.28ca54f5.js"
+  },
+  {
+    "revision": "f5bf3719c32532e1d63022f00cd319d6",
+    "url": "/wow-tutors/static/media/wow_logo.f5bf3719.png"
+  }
+]);
