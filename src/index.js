2b16a31a5128fda6dca0aca1533009dde155a799
akshayDatacode <[akshaywebllisto@gmail.com]>
2020-06-06 22:25:24 +0530

    Domain Setup try 3

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
   </Router>,
   document.getElementById("root")
