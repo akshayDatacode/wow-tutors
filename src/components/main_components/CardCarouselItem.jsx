dbccdb864fd379b92beffe05d82462b3c76eccd9
dhanwebllisto <dhanshreewebllisto01@gmail.com>
2020-06-07 18:42:17 +0530

    testimonial-img.css is added in assets and cardcarouselitem.jsx is modified

--- src/components/main_components/CardCarouselItem.jsx
+++ src/components/main_components/CardCarouselItem.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import { Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPrint, faUserNinja } from "@fortawesome/free-solid-svg-icons";
+import { faPrint, faUserNinja } from "@fortawesome/free-solid-svg-icons";                                                                                                                                                                                                                                                                                                                                                                                                             
+import "../../assets/css/Testimonial-img.css";
 class CardCarouselItem extends Component {
   state = {};
   render() {
@@ -12,8 +13,7 @@ class CardCarouselItem extends Component {
           style={{ background: "#43413d", color: "#f4cb58" }}
           className=" card text-center shadow mt-md-2 pl-md-5 pr-md-5 pt-md-3 pl-5 pr-5 p-3  m-3"
         >
-          <img
-            className=" card-img d-block w-100 circle p-sm-0  "
+          <img id="testimonial-img"
             style={{ borderRadius: "100%" }}
             src={require("../../assets/images/" + this.props.imgsource)}
             alt="First slide"
@@ -26,5 +26,5 @@ class CardCarouselItem extends Component {
     );
   }
 }
-
+                                                                                                                                                                                    
 export default CardCarouselItem;
